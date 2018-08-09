0e03eb6bb5c7330a2f5d5b80ee8db5b41bc9e89d
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-08-09 13:28:02 -0400

    Auto_selection

---
+++ Basics/Calender_Selection.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import time
+
+class CalenderSelection():
+
+    def test1(self):
+        driverLocation = "C:\\Users\\Chaithanya\\Documents\\chromedriver.exe"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        baseurl = "http://www.expedia.com"
+        driver = webdriver.Chrome(driverLocation)
+        driver.maximize_window()
+        driver.get(baseurl)
+        driver.implicitly_wait(3)
+        #Click flight tab
+        driver.find_element_by_id("tab-flight-tab-hp").click()
+        #Find departing field
+        departingfield = driver.find_element_by_id("flight-departing-hp-flight")
+        #click departing field
+        departingfield.click()
+        #Find the date to select
+        dateToSelect = driver.find_element(By.CSS_SELECTOR,".datepicker-cal .datepicker-cal-month:nth-child(4) tr:nth-of-type(5) .notranslate:nth-of-type(6) [type]")
+        dateToSelect.click()
+        time.sleep(3)
+        driver.quit()
+
+    def test2(self):
+        driverLocation = "C:\\Users\\Chaithanya\\Documents\\chromedriver.exe"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        baseurl = "http://www.expedia.com"
+        driver = webdriver.Chrome(driverLocation)
+        driver.maximize_window()
+        driver.get(baseurl)
+        driver.implicitly_wait(3)
+        # Click flight tab
+        driver.find_element_by_id("tab-flight-tab-hp").click()
+        # Find departing field
+        driver.find_element_by_id("flight-departing-hp-flight").click()
+        calMonth= driver.find_element(By.XPATH,"//div[@id='flight-departing-wrapper-hp-flight']/div[@class='datepicker-dropdown']")
+        allvalidDates = calMonth.find_elements(By.TAG_NAME, "a")
+
+        time.sleep(2)
+
+        for date in allvalidDates:
+
+            if date.text == "31":
+                date.click()
+                break
+
+
+
+
+cb = CalenderSelection()
+cb.test2()
