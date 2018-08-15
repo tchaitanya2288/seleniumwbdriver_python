d170a7d54625e570e4e1b2d4dd807c3311d6d957
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-08-15 09:58:06 -0400

    javascript_execution

---
+++ Advanced/javascript_execution.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+class JavascriptExecution():
+
+    def test(self):
+        driverLocation = "C:\\Users\\Chaithanya\\Documents\\chromedriver.exe"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        driver = webdriver.Chrome(driverLocation)
+        # baseUrl = "https://letskodeit.teachable.com/"
+        # driver.get(baseUrl)
+        # driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.execute_script("window.location = 'https://learn.letskodeit.com/p/practice';")
+        element = driver.execute_script("return document.getElementById('name');")
+        element.send_keys("Test")
+        time.sleep(5)
+
+
+ff = JavascriptExecution()
+ff.test()
