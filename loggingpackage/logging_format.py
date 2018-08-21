9d159ac5e0ad21a27e7344bce13c998fb717ce40
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-08-21 17:02:31 -0400

    logging_format

---
+++ loggingpackage/logging_format.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',datefmt='%m%d%Y %I:%M:%S %p',level=logging.DEBUG)
+logging.warning("warning message")
+logging.info("info message")
+logging.error("error message")
