75029da68b992c5eb8a76e105ef23705e4c56257
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-08-22 15:39:21 -0400

    testcase_demo

---
+++ unittestpackage/test_case_demo1.py
@@ -0,0 +1,31 @@
+import unittest
+
+class TestCaseDemo1(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("*#" * 30)
+        print("I will run only once before every test")
+        print("#*" * 30)
+
+    def setUp(self):
+        print("I will run once before every test")
+
+    def test_methodA(self):
+        print("Running method A")
+
+    def test_methodB(self):
+        print("running method B")
+
+    def tearDown(self):
+        print("I will run after every test")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("*#" * 30)
+        print("I will run only once after every test")
+        print("#*" * 30)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
